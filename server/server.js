2ce990242b3b8e69c93ece816a133b4357afcb01
Mifrah Malik <mifrah.malik@gmail.com>
2023-03-01 14:58:39 -0500

    all my commments for understanding

--- server/server.js
+++ server/server.js
@@ -9,10 +9,58 @@ const PORT = 8085; // 8085 is the port for backend
 app.use(cors());
 app.use(express.json());
 
-// creates an endpoint for the route/api
+//hard coding data inside server.js
+const coord = {
+  lon: -73.5387,
+  lat: 41.0534
+  weather: [{ 
+      id: 803,
+      main: "Clouds",
+      description: "broken clouds",
+      icon: "04d",
+      base: "stations",
+      main: {
+          temp: 5.2,
+          feels_like: 2.68,
+          temp_min: 3.54,
+          pressure: 1016,
+          humidity: 65
+          },
+      visibility: 10000,
+      wind: {
+              speed: 3.09,
+              deg: 160
+          },
+      clouds: {
+          all: 75
+      },
+      dt: 1677693199,
+      sys: {
+          type: 2,
+          id: 2010292,
+          country: "US",
+          sunrise: 1677670142,
+          sunset: 1677710664
+          },
+      timezone: -18000,
+      id: 4843564,
+      name: "Stamford",
+      cod: 200
+      }
+  ]
+}
+// // creates an endpoint for the route/api
+// app.get("/", (req, res) => {
+//   res.json({ message: "Hello from My template ExpressJS using and API Key" });
+// });
+
+
+//copying example from last week with books.js
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from My template ExpressJS using and API Key" });
+  res.json(coord); //instead of line above, need to do res.json
+  //res.send("Hello"); // i think this line is not necessary
 });
+
 
 // // creates an endpoint for the route /api/weather // endpoint is the weather endpoint (this is the invoice)
 // app.get("/api/weather/", (req, res) => {
