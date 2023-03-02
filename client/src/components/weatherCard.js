a0b650d9f1eba3b11c789be93d84fe3a4d171448
Mifrah Malik <mifrah.malik@gmail.com>
2023-03-02 13:17:30 -0500

    added in humidity, temp_min to count towards 3 more pieces of extra information - already had country in which wasn't listed

--- client/src/components/weatherCard.js
+++ client/src/components/weatherCard.js
@@ -2,31 +2,67 @@ const WeatherCard = (props) => {
 
 
     return (
-        <div className="weather-card">
-            <div className="result">
-                <p>City: <span className="data">
-                    {props.data.weather[0].name}, {props.data.weather[0].sys.country}
-                </span>
-                </p>
-                <p>Description: <span className="data">
-                {props.data.weather[0].description}
-              </span>
-            </p>
-            <img src={`http://openweathermap.org/img/wn/${props.data.weather[0].icon}@4x.png`} alt={"Icon from Open Weather Api"}/>
-            <p>
-              Temperature: <span className="data">
-                {props.data.weather[0].main.temp} <sup>o</sup>C
-              </span>
-            </p>
-            <p>
-              Feels Like: <span className="data">
-                {props.data.weather[0].main.feels_like} <sup>o</sup>C
-              </span>
-            </p>
+        // <div className="weather-card">
+        //     <div className="result">
+        //         <p>City: <span className="data">
+        //             {props.data.weather[0].name}, {props.data.weather[0].sys.country}
+        //         </span>
+        //         </p>
+        //         <p>Description: <span className="data">
+        //         {props.data.weather[0].description}
+        //       </span>
+        //     </p>
+        //     <img src={`http://openweathermap.org/img/wn/${props.data.weather[0].icon}@4x.png`} alt={"Icon from Open Weather Api"}/>
+        //     <p>
+        //       Temperature: <span className="data">
+        //         {props.data.weather[0].main.temp} <sup>o</sup>C
+        //       </span>
+        //     </p>
+        //     <p>
+        //       Feels Like: <span className="data">
+        //         {props.data.weather[0].main.feels_like} <sup>o</sup>C
+        //       </span>
+        //     </p>
 
-            </div>
+        //     </div>
+
+        // </div>
+        <div className="weather-card">
+        <div className="result">
+            <p>City: <span className="data">
+                {props.data.weather[0].name}, {props.data.weather[0].sys.country}
+            </span>
+            </p>
+            <p>Description: <span className="data">
+            {props.data.weather[0].description}
+          </span>
+        </p>
+        <img src={`http://openweathermap.org/img/wn/${props.data.weather[0].icon}@4x.png`} alt={"Icon from Open Weather Api"}/>
+        <p>
+          Temperature: <span className="data">
+            {props.data.weather[0].main.temp} <sup>o</sup>C
+          </span>
+        </p>
+        <p>
+          Temperature Minimum: <span className="data">
+            {props.data.weather[0].main.temp_min} <sup>o</sup>C
+          </span>
+        </p>
+        <p></p>
+        <p>
+          Feels Like: <span className="data">
+            {props.data.weather[0].main.feels_like} <sup>o</sup>C
+          </span>
+        </p>
+        <p>
+          Humidity: <span className="data">
+            {props.data.weather[0].main.humidity} <sup>o</sup>C
+          </span>
+        </p>
 
         </div>
+
+    </div>
     )
 }
 
