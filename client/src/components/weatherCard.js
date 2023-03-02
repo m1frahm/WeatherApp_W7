5573c24044bbf873e3a838564e78a6eaed370b8a
Mifrah Malik <mifrah.malik@gmail.com>
2023-03-02 14:16:50 -0500

    made changes to weatherCard to data.data.name for object instead of data.weather[0].name

--- client/src/components/weatherCard.js
+++ client/src/components/weatherCard.js
@@ -31,40 +31,40 @@ const WeatherCard = (props) => {
         <p>
           City:{" "}
           <span className="data">
-            {props.data.weather[0].name}, {props.data.weather[0].sys.country}
+            {props.data.data.name}, {props.data.data.sys.country}
           </span>
         </p>
         <p>
           Description:{" "}
-          <span className="data">{props.data.weather[0].description}</span>
+          <span className="data">{props.data.data.description}</span>
         </p>
         <img
-          src={`http://openweathermap.org/img/wn/${props.data.weather[0].icon}@4x.png`}
+          src={`http://openweathermap.org/img/wn/${props.data.data.icon}@4x.png`}
           alt={"Icon from Open Weather Api"}
         />
         <p>
           Temperature:{" "}
           <span className="data">
-            {props.data.weather[0].main.temp} <sup>o</sup>C
+            {props.data.data.main.temp} <sup>o</sup>C
           </span>
         </p>
         <p>
           Temperature Minimum:{" "}
           <span className="data">
-            {props.data.weather[0].main.temp_min} <sup>o</sup>C
+            {props.data.data.main.temp_min} <sup>o</sup>C
           </span>
         </p>
         <p></p>
         <p>
           Feels Like:{" "}
           <span className="data">
-            {props.data.weather[0].main.feels_like} <sup>o</sup>C
+            {props.data.data.main.feels_like} <sup>o</sup>C
           </span>
         </p>
         <p>
           Humidity:{" "}
           <span className="data">
-            {props.data.weather[0].main.humidity} <sup>o</sup>C
+            {props.data.data.main.humidity} <sup>o</sup>C
           </span>
         </p>
       </div>
